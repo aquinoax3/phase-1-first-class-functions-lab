38dfb1204c59f673cd3acda69f8106f7df8a3bd6
Axel Aquino <aquino.axel@gmail.com>
2022-02-01 13:46:41 -0500

    commit tree 49607536de0f4a47040bed0d92152fa6fcee4809

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
-// Code your solution in this file!
+
+
+
+
+const returnFirstTwoDrivers = (function (driver1) {
+    return driver1.slice(0,2);
+
+});
+
+
+const returnLastTwoDrivers = (function (driver1){
+    return driver1.slice(-2);
+
+});
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(integer) {
+    return (function (fare) {
+        return fare * integer
+    });
+}
+
+
+const fareDoubler = function fareDoubler(fare) {
+    return createFareMultiplier(2)(fare); 
+} 
+
+const fareTripler = function fareTripler(fare) {
+    return createFareMultiplier(3)(fare);
+
+}
+
+
+//const fareDoubler = createFareMultiplier(2)
+
+//const fareTripler =  createFareMultiplier(3)
+
+
+function selectDifferentDrivers(arrDrivers,returnFirstTwoDrivers) {
+    return returnFirstTwoDrivers(arrDrivers);
+}
+
+
